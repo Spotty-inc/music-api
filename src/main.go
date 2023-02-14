4f6772906d7d008eded9ba9bb0610a4f878c822a
Samuel Langley <samuellangley@intelligencefusion.co.uk>
2023-02-14 00:11:09 +0000

    add token handling to spotify api

--- src/main.go
+++ src/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"log"
+	"context"
 	"net/http"
+	"fmt"
+	"os"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/zmb3/spotify/v2"
+	"github.com/zmb3/spotify/v2/auth"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2/clientcredentials"
+	"golang.org/x/oauth2"
 )
 
 type Song struct {
@@ -15,6 +23,22 @@ type Song struct {
 }
 
 var Songs []Song
+
+func getToken() *oauth2.Token {
+	SPOTIFY_ID := os.Getenv("SPOTIFY_ID")
+	SPOTIFY_SECRET := os.Getenv("SPOTIFY_SECRET")
+	ctx := context.Background()
+	config := &clientcredentials.Config{
+		ClientID: SPOTIFY_ID,
+		ClientSecret: SPOTIFY_SECRET,
+		TokenURL: spotifyauth.TokenURL,
+	}
+	token, err := config.Token(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+	return token
+}
 
 func healthCheck(w http.ResponseWriter, r *http.Request){
 	log.Println("Endpoint Hit: healthCheck")
@@ -30,7 +54,26 @@ func healthCheck(w http.ResponseWriter, r *http.Request){
 	return
 }
 
+func returnRandomSong(w http.ResponseWriter, r *http.Request){
+	token := getToken()
+	ctx := context.Background()
+	httpClient := spotifyauth.New().Client(ctx, token)
+	client := spotify.New(httpClient)
+	results, err := client.Search(ctx, "One Dance", spotify.SearchTypeTrack)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if results.Tracks != nil {
+		fmt.Println("Songs:")
+		for _, item := range results.Tracks.Tracks {
+			fmt.Println("   ", item.Name)
+		}
+	}
+
+}
+
 func returnSingleSong(w http.ResponseWriter, r *http.Request){
+	// token := getToken()
 	log.Println("Endpoint Hit: returnSingleSong")
 	vars := mux.Vars(r)
 	key := vars["id"]
@@ -54,7 +97,8 @@ func requestHandler(){
 	router.HandleFunc("/health", healthCheck)
 	router.HandleFunc("/songs", returnAllSongs)
 	router.HandleFunc("/song/{id}", returnSingleSong)
-	log.Fatal(http.ListenAndServe(":10000", router))
+	router.HandleFunc("/random", returnRandomSong)
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
 func main(){
@@ -62,5 +106,9 @@ func main(){
 		Song{Id: "1", Title: "Titanium", Artist: "Dave", Popularity: 60},
 		Song{Id: "2", Title: "Low", Artist: "Sza", Popularity: 75},
 	}
+	err := godotenv.Load()
+  	if err != nil {
+    log.Fatal("Error loading .env file")
+  	}
 	requestHandler()
 }
